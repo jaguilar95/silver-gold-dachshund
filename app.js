8db565868dadf79d509c55f368fb4197820e541a
Leonardo <josue.aguilar1995@gmail.com>
2022-06-18 10:12:18 -0500

    pseudo code for acceptance criteria

---
+++ app.js
@@ -0,0 +1,53 @@
+const inquirer = require("inquirer");
+
+const promptStart = () => {
+  /*
+    What would you like to do?
+        * View all departments
+        * View all roles
+        * View all Employees
+        * Add a department
+        * Add a role
+        * Add an employee
+        * Update an employee role
+    
+    Choosing an to view all departments, show:
+        * Department names
+        * Department ids
+    
+    Choosing to view all roles, show:
+        * Job title
+        * Role id
+        * Department belonging to
+        * Salary (all for role)
+    
+    Choosing to view all employees, show:
+        * Employee data
+        * Employee id
+        * First name
+        * Last name
+        * Job Title
+        * Department
+        * Salary
+        * Manager
+    
+    Choosing to add a department, ask:
+        * Name of department
+    
+    Choosing to add a role, ask:
+        * Title
+        * Salary
+        * Department
+    
+    Choosing to add an employee, ask:
+        * First name
+        * Last name
+        * Role
+        * Manager
+    
+    Choosing to update an employee's role, ask:
+        * To select an employee
+        * Update role
+
+    */
+};
